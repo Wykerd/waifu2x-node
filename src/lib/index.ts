b33273134247d86bfc7f6373dd9644d321346962
Daniel Wykerd <danielwykerd@gmail.com>
2020-05-17 17:00:46 +0200

    Fix lib path resolves - Module now working on Windows

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import os from "os";
 import path from "path";
 
 if (os.platform() === "win32") {
-    process.env.PATH = `${process.env.PATH};${path.resolve(__dirname, 'vendor/w2xc')};${path.resolve(__dirname, 'vendor/opencv/build/x64/vc15/bin')}`;
+    process.env.PATH = `${process.env.PATH};${path.resolve(__dirname, '../vendor/w2xc')};${path.resolve(__dirname, '../vendor/opencv/build/x64/vc15/bin')}`;
 }
 
 export const DEFAULT_MODELS_DIR = path.resolve(__dirname, '../vendor/w2xc/models_rgb');
